12e84035d827417ea09f79d7ba66beecf0faf64c
aniketgaikwad4635 <aniketgaikwad4635@gmail.com>
2021-05-22 14:54:55 +0530

    initial commit
    project setup

---
+++ trial/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,83 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int srNo;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private int age;
+	
+	@Column(nullable = false)
+	private String gender;
+	
+	@Column(nullable = false)
+	private String mobile;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(String name, int age, String gender, String mobile) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.mobile = mobile;
+	}
+
+	public int getSrNo() {
+		return srNo;
+	}
+
+	public void setSrNo(int srNo) {
+		this.srNo = srNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	
+	
+	
+}
